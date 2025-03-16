bf6a8c788c735db6654d04bfa90e085d03942f90
Shubho Bhadra <107258681+Shubho97@users.noreply.github.com>
2025-03-17 00:17:37 +0530

    FastAPI project commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from typing import Optional
 from uuid import UUID
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI, HTTPException # type: ignore
 from pydantic import BaseModel
 import uvicorn
 
+
 app = FastAPI()
 
-@app.get("/about")
-def about():
-    return {"data": {"name": "Shubho"}}
 class Book(BaseModel):
     id: UUID
     title: str
@@ -17,4 +15,46 @@ class Book(BaseModel):
 
 BOOKS= []
 
+#TO DISPLAY THE BOOK DATA IF PRESENT
+@app.get("/")
+def about():
+    return BOOKS
 
+#TO STORE THE BOOK DATA
+@app.post("/")
+def create_book(book: Book):
+    BOOKS.append(book)
+    return book
+
+
+#TO UPDATE THE DETAILS
+@app.put("/{book_id}")
+def update_book(book_id: UUID, book: Book):
+    counter= 0
+
+    for x in BOOKS:
+        counter +=1
+        if x.id== book_id:
+            BOOKS[counter - 1] = book
+            return BOOKS[counter-1]
+    
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} : Does not exist"
+    )
+
+#TO DELETE BOOK DATA
+@app.delete("/{book_id}")
+def delete_book(book_id: UUID, book: Book):
+    counter= 0
+
+    for x in BOOKS:
+        counter +=1
+        if x.id== book_id:
+            del BOOKS[counter - 1]
+            return f"ID: {book_id} is deleted"
+    
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} : Does not exist"
+    )
